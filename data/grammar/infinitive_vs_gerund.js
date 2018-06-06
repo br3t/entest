4f78f65ae8c64c30202b60c969fde2547876f8c3
br3t <bret.snk@gmail.com>
2018-06-07 00:26:41 +0300

    Add some topic

---
+++ data/grammar/infinitive_vs_gerund.js
@@ -0,0 +1,121 @@
+var test = test || {};
+test.infinitive_vs_gerund = {
+	title: '',
+	titles: {
+		ru: 'to be или being',
+		en: 'Infinitive or Gerund'
+	},
+	vars: ['to do', 'doing', 'do'],
+	units: [{
+			txt: "I decided {to do}"
+		},
+		{
+			txt: "He offered {to do}"
+		},
+		{
+			txt: "She stopped {doing}"
+		},
+		{
+			txt: "He helps {do}"
+		},
+		{
+			txt: "She forgot {to do}"
+		},
+		{
+			txt: "I hope {to do}"
+		},
+		{
+			txt: "They learn {to do}"
+		},
+		{
+			txt: "We need {to do}"
+		},
+		{
+			txt: "You plan {to do}"
+		},
+		{
+			txt: "He pretends {to do}"
+		},
+		{
+			txt: "She promised {to do}"
+		},
+		{
+			txt: "I remember {to do}"
+		},
+		{
+			txt: "We try {to do}"
+		},
+		{
+			txt: "I want {to do}"
+		},
+		{
+			txt: "He would like {to do}"
+		},
+		{
+			txt: "We started {to do}"
+		},
+		/*{
+			txt: "We started {to do}|ing"
+		},*/
+		{
+			txt: "I enjoy {doing}"
+		},
+		{
+			txt: "He finished {doing}"
+		},
+		{
+			txt: "I go on {doing}"
+		},
+		{
+			txt: "She hates {doing}"
+		},
+		{
+			txt: "We like {doing}"
+		},
+		{
+			txt: "You love {doing}"
+		},
+		{
+			txt: "He doesn't mind {doing}"
+		},
+		{
+			txt: "You spend time {doing}"
+		},
+		{
+			txt: "She stopped {doing}"
+		},
+		{
+			txt: "They feel like {doing}"
+		},
+		{
+			txt: "I must {do}"
+		},
+		{
+			txt: "She should {do}"
+		},
+		{
+			txt: "You will {do}"
+		},
+		{
+			txt: "He can {do}"
+		},
+		{
+			txt: "They would {do}"
+		},
+		{
+			txt: "It's easy {to do}"
+		},
+		{
+			txt: "You may {do}"
+		},
+		{
+			txt: "It's important {to do}"
+		},
+		{
+			txt: "It's funny {to do}"
+		},
+		{
+			txt: "It's interesting {to do}"
+		}
+	]
+};
